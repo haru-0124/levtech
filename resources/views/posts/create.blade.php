3b980ada1fd44fefa85dc89e213b2a67fcef1229
haru-0124 <k.haruto0124@au.com>
2025-01-16 00:12:37 +0000

    iroiro

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,21 +5,36 @@
         <title>Blog</title>
     </head>
     <body>
-        <h1>Blog Name</h1>
-        <form action="/posts" method="POST">
-            @csrf
-            <div class="title">
-                <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
-                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
-            </div>
-            <div class="body">
-                <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
-                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
-            </div>
-            <input type="submit" value="保存"/>
-        </form>
-        <div class="back">[<a href="/">back</a>]</div>
+        <x-app-layout>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Index Page') }}
+                </h2>
+            </x-slot>
+            <h1>Blog Name</h1>
+            <form action="/posts" method="POST">
+                @csrf
+                <div class="title">
+                    <h2>Title</h2>
+                    <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                    <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+                </div>
+                <div class="category">
+                    <h2>Category</h2>
+                    <select name="post[category_id]">
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="body">
+                    <h2>Body</h2>
+                    <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+                    <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+                </div>
+                <input type="submit" value="保存"/>
+            </form>
+            <div class="back">[<a href="/">back</a>]</div>
+        </x-app-layout>
     </body>
 </html>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,21 +5,28 @@
         <title>Blog</title>
     </head>
     <body>
-        <h1 class="title">編集画面</h1>
-        <div class="content">
-            <form action="/posts/{{ $post->id }}" method="POST">
-                @csrf
-                @method('PUT')
-                <div class="content__title">
-                    <h2>タイトル</h2>
-                    <input type='text' name='post[title]' value="{{ $post->title }}">
-                </div>
-                <div class="content__body">
-                    <h2>本文</h2>
-                    <input type='text' name='post[body]' value="{{ $post->body }}">
-                </div>
-                <input type="submit" value="保存">
-            </form>
-        </div>
+        <x-app-layout>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Index Page') }}
+                </h2>
+            </x-slot>
+            <h1 class="title">編集画面</h1>
+            <div class="content">
+                <form action="/posts/{{ $post->id }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="content__title">
+                        <h2>タイトル</h2>
+                        <input type='text' name='post[title]' value="{{ $post->title }}">
+                    </div>
+                    <div class="content__body">
+                        <h2>本文</h2>
+                        <input type='text' name='post[body]' value="{{ $post->body }}">
+                    </div>
+                    <input type="submit" value="保存">
+                </form>
+            </div>
+        </x-app-layout>
     </body>
 </html>
